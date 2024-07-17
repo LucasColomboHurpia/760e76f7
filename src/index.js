3a4f3f6ffa8766b39e188effd268fb31f2ba4a48
Lucas <lucas.c.hurpia@gmail.com>
2024-07-17 04:33:23 -0700

    adds better setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import React from 'react';
+import { createRoot, Switch } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/index.css';
+import './css/app.css'
+import './css/body.css'
+import './css/header.css'
 
-import App from './App.jsx';
+import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App id='#app' />
+  </React.StrictMode>
+);
+
+//https://github.com/speer-technologies/aircall
+//https://docs.google.com/forms/d/e/1FAIpQLSeYU8P-RlgCcl8eI8vttI7AI-8OfywP-YFzkWzML_apXj4bSA/viewform
+//https://miro.com/app/board/uXjVK9UItTc=/
