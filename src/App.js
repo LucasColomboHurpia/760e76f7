467670744eb77bec41c11c86f80e8d9bd65fa98b
Lucas <lucas.c.hurpia@gmail.com>
2024-07-18 06:46:25 -0700

    visual bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink, useNavigate } from 'react-router-dom';
 import ActivityFeed from './components/ActivityFeed.js';
 import ActivityDetail from './components/ActivityDetail.js';
 import AllCalls from './components/AllCalls.js';
@@ -7,13 +7,13 @@ import Logo from './components/Logo.js';
 import { CallProvider } from './context/CallContext';
 import { DarkModeProvider, useDarkMode } from './context/DarkModeContext';
 import PageTransition from './components/PageTransition.js';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 const DarkModeToggle = () => {
     const { isDarkMode, toggleDarkMode } = useDarkMode();
 
     return (
-        <button onClick={toggleDarkMode} className="btn  dark-mode-toggle">
+        <button onClick={toggleDarkMode} className="btn btn-link dark-mode-toggle">
             {isDarkMode ? <FaSun /> : <FaMoon />}
         </button>
     );
@@ -24,34 +24,46 @@ const App = () => {
         <DarkModeProvider>
             <CallProvider>
                 <Router>
-                    <div className="container">
-                        <nav className="navbar navbar-expand-lg navbar-light bg-light myNavbar shadow mb-4">
-                            <div className="d-flex align-items-center myNavbarInner pt-2 pb-2">
-                                <a className="navbar-brand" href="/">
-                                    <Logo />
-                                </a>
-                                <div className="navbar-nav ml-auto justify-content-center align-items-stretch">
-                                    <NavLink className="nav-link text-center" to="/" exact activeClassName="active">
-                                        Inbox
-                                    </NavLink>
-                                    <NavLink className="nav-link text-center" to="/all-calls" activeClassName="active">
-                                        All calls
-                                    </NavLink>
-                                </div>
-                                <DarkModeToggle />
-                            </div>
-                        </nav>
-                        <PageTransition>
-                            <Routes>
-                                <Route path="/" element={<ActivityFeed />} />
-                                <Route path="/all-calls" element={<AllCalls />} />
-                                <Route path="/detail/:id" element={<ActivityDetail />} />
-                            </Routes>
-                        </PageTransition>
-                    </div>
+                    <AppContent />
                 </Router>
             </CallProvider>
         </DarkModeProvider>
+    );
+};
+
+const AppContent = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/');
+    }, [navigate]);
+
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light myNavbar shadow mb-4">
+                <div className="d-flex align-items-center myNavbarInner pt-2 pb-2">
+                    <a className="navbar-brand" href="/">
+                        <Logo />
+                    </a>
+                    <div className="navbar-nav ml-auto justify-content-center align-items-stretch">
+                        <NavLink className="nav-link text-center" to="/" exact activeClassName="active">
+                            Inbox
+                        </NavLink>
+                        <NavLink className="nav-link text-center" to="/all-calls" activeClassName="active">
+                            All calls
+                        </NavLink>
+                    </div>
+                    <DarkModeToggle />
+                </div>
+            </nav>
+            <PageTransition>
+                <Routes>
+                    <Route path="/" element={<ActivityFeed />} />
+                    <Route path="/all-calls" element={<AllCalls />} />
+                    <Route path="/detail/:id" element={<ActivityDetail />} />
+                </Routes>
+            </PageTransition>
+        </div>
     );
 };
 
